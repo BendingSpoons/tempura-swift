5d5837fe4a83bac87d2197734efb95b1f0bbd789
Andrea De Angelis <smaramba83@gmail.com>
2017-08-28 14:21:03 +0200

    Fix block based target/action for UIControl

--- Tempura/Extensions/UIControl+TargetActionable.swift
+++ Tempura/Extensions/UIControl+TargetActionable.swift
@@ -8,30 +8,69 @@
 
 import Foundation
 
-class ActionTrampoline<T>: NSObject {
+class Trampoline: NSObject {
+  @objc func action(sender: UIControl) {}
+}
+
+class ActionTrampoline<T>: Trampoline {
   var act: (T) -> ()
   
   init(action: @escaping (T) -> ()) {
     self.act = action
   }
   
-  @objc func action(sender: UIControl) {
+  override func action(sender: UIControl) {
     act(sender as! T)
+  }
+}
+
+extension UIControlEvents {
+  var number: NSNumber {
+    return NSNumber(integerLiteral: Int(self.rawValue))
   }
 }
 
 public protocol TargetActionable {}
 
+fileprivate var actionTrampolinesKey = "targetactionable_action_trampolines_key"
+
 public extension TargetActionable where Self: UIControl {
-  func on(_ event: UIControlEvents, _ action: @escaping (Self) -> ()) {
+  
+  mutating func on(_ event: UIControlEvents, _ action: @escaping (Self) -> ()) {
+    if let oldTrampoline = self.actionTrampolines?[event.number] as? Trampoline {
+      self.removeTarget(oldTrampoline, action: #selector(oldTrampoline.action), for: event)
+    }
+    if self.actionTrampolines == nil {
+      self.actionTrampolines = NSMutableDictionary()
+    }
     let trampoline = ActionTrampoline(action: action)
     self.addTarget(trampoline, action: #selector(trampoline.action), for: event)
-    // just needed to retain the trampoline to keep it alive
-    objc_setAssociatedObject(self, "\(event)", trampoline, .OBJC_ASSOCIATION_RETAIN)
+    self.actionTrampolines?[event.number] = trampoline
   }
+  
+  private var actionTrampolines: NSMutableDictionary? {
+    get {
+      if let actionTrampolines = objc_getAssociatedObject(self, &actionTrampolinesKey) as? NSMutableDictionary {
+        return actionTrampolines
+      }
+      return nil
+    }
+    
+    set {
+      if let newValue = newValue {
+        objc_setAssociatedObject(
+          self,
+          &actionTrampolinesKey,
+          newValue as NSMutableDictionary?,
+          .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+        )
+      }
+    }
+  }
+  
 }
 
-let tapHandlerKey = UnsafeMutablePointer<Int8>.allocate(capacity: 1)
+private let tapHandlerKey = UnsafeMutablePointer<Int8>.allocate(capacity: 1)
 
 public extension TargetActionable where Self: UIBarButtonItem {
   func onTap(_ action: @escaping (Self) -> ()) {
