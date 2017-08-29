93ecebbdd2c88f6367f84064f21f3e02c43b108f
Andrea De Angelis <smaramba83@gmail.com>
2017-08-29 16:30:30 +0200

    Fix Modals

--- Tempura/Navigation/Modals.swift
+++ Tempura/Navigation/Modals.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+
+fileprivate var toBeDismissedKey = "view_controller_to_be_dismissed"
+
 public extension UIViewController {
   public func tempuraPresent(viewController: UIViewController, animated: Bool, completion: (() -> Void)?) {
     // check if we are already presenting something, if so, ask the presented to present the viewController
@@ -25,7 +28,7 @@ public extension UIViewController {
     // check if the viewController is presenting something (not marked as toBeDismissed)
     // if so, we cannot dismiss it (otherwise it will dismiss all the segues)
     // but we mark it as `toBeDismissed`
-    if let presentedViewController = viewController.presentedViewController && !presentedViewController.toBeDismissed {
+    if let presentedViewController = viewController.presentedViewController, !presentedViewController.toBeDismissed {
       viewController.toBeDismissed = true
     } else {
       // this viewController can be dismissed now, let's check if the parent is marked as `toBeDismissed`
@@ -36,6 +39,23 @@ public extension UIViewController {
       } else {
         viewController.dismiss(animated: animated, completion: completion)
       }
+    }
+  }
+  
+  public var toBeDismissed: Bool {
+    get {
+      let value = objc_getAssociatedObject(self, &toBeDismissedKey) as! NSNumber
+      return value.boolValue
+    }
+    
+    set {
+      let value = NSNumber(booleanLiteral: newValue)
+      objc_setAssociatedObject(
+        self,
+        &toBeDismissedKey,
+        value,
+        .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+      )
     }
   }
   
