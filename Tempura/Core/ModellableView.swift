f8465affe5ab4ad8eab52dc8ab40c93db8ff711f
Andrea De Angelis <smaramba83@gmail.com>
2018-02-21 10:19:20 +0100

    Add documentation for core files

--- Tempura/Core/ModellableView.swift
+++ Tempura/Core/ModellableView.swift
@@ -9,18 +9,108 @@
 import Foundation
 import UIKit
 
-/// the ModellableView protocol defines the structure of a View that is more complex than a simple reusable View
-/// (for which we use the simpler `View` protocol).
-/// A perfect candidate for this protocol is a View that contains specific Domain level knowledge and
-/// it is more natural to be updated using the concept of a ViewModel instead of a set of properties.
-
-
 fileprivate var modelWrapperKey = "modellableview_model_wrapper_key"
+
+/// Mixin protocol for UIView subclasses based on the same SSUL lifecycle of `View`. Conforming to `ModellableView`, a UIView will get a `model: ViewModel` property
+/// and the `update(oldModel: ViewModel?)` will be automatically called each time the model property will change.
+/// If your UIView is simple and you don't want to use a ViewModel, refer to the `View` protocol instead.
+
+/// ## Overview
+/// The `View` protocol is good enough for small reusable UI elements that can be manipulated through **properties**.
+/// There are a couple of drawbacks to this approach:
+/// - it's not easy to test UI elements
+/// - in the `View.update()` phase we don't know the actual property that is changed, meaning that we cannot reason in terms of differences from the old values
+/// - changing two or more properties at the same time will trigger two or more updates.
+///
+/// To solve all of these issues we introduce the concept of `ViewModel`.
+/// A ViewModel is a struct that contains all the properties that define the state of the View.
+
+/// ```swift
+///    struct ContactViewModel: ViewModel {
+///      var name: String = "John"
+///      var lastName: String = "Doe"
+///    }
+/// ```
+
+/// A `ModellableView` then is a special case of `View` that is using a ViewModel to represent its state.
+/// All the Setup, Style and Layout phases described in `View` are still in use, the only difference is that
+/// the Update method of the ModellableView is getting an `oldModel` parameter.
+
+/// ```swift
+///    struct ContactView: ModellableView {
+///
+///      // subviews to create the UI
+///      private var title = UILabel()
+///      private var subtitle = UILabel()
+///
+///      // interactions
+///      var nameDidChange: ((String) -> ())?
+///      var lastNameDidChange: ((String) -> ())?
+///
+///      override init(frame: CGRect = .zero) {
+///        super.init(frame: frame)
+///        self.setup()
+///        self.style()
+///      }
+///
+///      func setup() {
+///        // define the subviews that will make up the UI
+///        self.addSubview(self.title)
+///        self.addSubview(self.subtitle)
+///
+///        self.title.on(.didEndEditing) { [weak self] label in
+///          self?.nameDidChange?(label.text)
+///        }
+///        self.subtitle.on(.didEndEditing) { [weak self] label in
+///          self?.lastNameDidChange?(label.text)
+///        }
+///      }
+///
+///      func style() {
+///        // define the default look and feel of the UI elements
+///      }
+///
+///      func update(oldModel: ContactViewModel?) {
+///        // update the UI based on the value of `self.model`
+///        // you can use `oldModel` to reason about diffs
+///        self.title.text = self.model.name
+///        self.subtitle.text = self.model.lastname
+///      }
+///
+///      func layoutSubviews() {
+///        // layout the subviews
+///      }
+///    }
+/// ```
+///
+/// Conforming to `ModellableView` will:
+/// - create the `model: ContactViewModel` variable automatically for you.
+/// - automatically call the `ModellableView.update(oldModel:)` method every time the model changes
+/// - allow to test the ViewModel instead of testing the ModellableView
+/// - include the oldModel inside the `ModellableView.update(oldModel:)` so that you can reason about diffs
+/// - allow to change more than one property and trigger only one update
+///
+/// ```swift
+///    public protocol ModellableView: View {
+///
+///      associatedtype VM: ViewModel
+///
+///      // the ViewModel of the View.
+///      // `update(oldModel: VM?)` will be called each time model will change
+///      var model: VM! { get set }
+///
+///      // the model is changed, update the View
+///      func update(oldModel: VM?)
+///    }
+/// ```
 
 public protocol ModellableView: View {
   associatedtype VM: ViewModel
   
-  /// the ViewModel of the View. Once changed, the `update(oldModel: VM?)` will be called
+  /// The ViewModel of the View. Once changed, the `update(oldModel: VM?)` will be called.
+  /// The model variable is automatically created for you once you conform to the ModellableView protocol.
+  /// Swift is inferring the Type through the `oldModel` parameter of the `update(oldModel: ViewModel?)` method
+  /// and we are adding the var exploiting a feature of the Objective-C runtime called [Associated Objects](http://nshipster.com/associated-objects/)
   var model: VM? { get set }
   
   /// the ViewModel is changed, update the View using the `oldModel` and the new `self.model`
--- Tempura/Core/ViewControllerWithLocalState.swift
+++ Tempura/Core/ViewControllerWithLocalState.swift
@@ -11,21 +11,134 @@ import UIKit
 import Katana
 import Chocolate
 
+/// Special case of a `ViewController` that contains a `LocalState`.
+
+/// ## Overview
+/// A `ViewController` is managing the UI of a screen, listening for Katana global state changes, keeping the UI updated
+/// and dispatching actions in response to user interactions in order to change the global state.
+///
+/// There are times when you have some kind of state information that is only specific to the screen managed
+/// by a ViewController, like for instance the item selected in a list.
+/// In this case, in order to avoid polluting the global state, you can represent that information inside
+/// a `LocalState` and promote that ViewController to be a `ViewControllerWithLocalState`.
+///
+/// A ViewControllerWithLocalState contains a `localState` variable that you can change directly in order
+/// to represent local state changes.
+/// The ViewControllerWithLocalState will be listening for changes of both global and local states, updating
+/// the UI using the appropriate `ViewModelWithLocalState`.
+///
+/// You can change the global state dispatching Katana actions like in every `ViewController`. You can change
+/// the `LocalState` manipulating directly the `localState` variable.
+///
+/// The lifecycle of a ViewControllerWithLocalState is the same as a normal `ViewController`, please refer to that
+/// for more details
+
+/// ```swift
+///    struct GlobalState: State {
+///      var todos: [String] = [
+///        "buy milk",
+///        "find a unicorn",
+///        "visit Rome"
+///      ]
+///    }
+/// ```
+
+/// ```swift
+///    struct RemoveTodo: AppAction {
+///      var index: Int
+///
+///      func updatedState(inout currentState: GlobalState) {
+///        currentState.todos.remove(at: index)
+///      }
+///    }
+/// ```
+
+/// ```swift
+///    struct ListLocalState: LocalState {
+///      var selectedIndex: Int
+///    }
+/// ```
+
+/// ```swift
+///    struct TodolistViewModel: ViewModelWithLocalState {
+///      var todos: [String]
+///      var selectedIndex: Int
+///
+///      init?(state: GlobalState?, localState: ListLocalState) {
+///        guard let state = state else { return nil }
+///        self.todos = state.todos
+///        self.selectedIndex = localState.selectedIndex
+///      }
+///    }
+/// ```
+
+/// ```swift
+///    class TodoListView: UIView, ViewControllerModellableView {
+///
+///      // subviews
+///      var todoListView = ListView()
+///
+///      // interactions
+///      var didTapToRemoveItem: ((Int) -> ())?
+///      var didSelectItem: ((Int) -> ())?
+///
+///      // setup
+///      func setup() {
+///        self.todoListView.on(.selection) { [unowned self] indexPath in
+///          self.didSelectItem?(indexPath.item)
+///        }
+///        self.todoListView.on(.deleteItem) { [unowned self] indexPath in
+///          seld.didTapRemoveItem?(indexPath.item)
+///        }
+///        self.addSubview(self.todoListView)
+///      }
+///
+///      // style
+///      func style() {
+///        self.backgroundColor = .white
+///        self.todoListView.backgroundColor = .white
+///      }
+///
+///      // update
+///      func update(oldModel: CounterViewModel?) {
+///        self.todoListView.source = model?.todos ?? []
+///        self.todoListView.selectedIndex = model?.todos
+///      }
+///
+///      // layout
+///      override func layoutSubviews() {
+///        self.todoListView.frame = self.bounds
+///      }
+///    }
+/// ```
+
+/// ```swift
+///    class CounterViewController: ViewController<CounterView> {
+///
+///    override func setupInteraction() {
+///      self.rootView.didTapRemoveItem = { [unowned self] index in
+///        self.dispatch(RemoveTodo(index: index))
+///      }
+///      self.rootView.didSelectItem = { [unowned self] index in
+///        self.localState.selectedIndex = index
+///      }
+///    }
+/// ```
 
 open class ViewControllerWithLocalState<V: ViewControllerModellableView & UIView>: ViewController<V> where V.VM: ViewModelWithLocalState {
   
-  /// the local state of this ViewController
+  /// The `LocalState` of this ViewController
   public var localState: V.VM.LS = V.VM.LS() {
     didSet {
       self.localStateDidChange()
     }
   }
   
-  /// this is the last value for state we observed, we are saving this to be able to update the ViewModel
-  /// when the local state changes and we are disconnected from the global state, we use this value as the global state
+  /// This is the last value for global state we observed, we are saving this to be able to update the `ViewModelWithLocalState`
+  /// when the local state changes and we are disconnected from the global state
   public var lastKnownState: V.VM.S?
   
-  /// the init of the view controller that will take the Store to perform the updates when the store changes
+  /// The init of the ViewController, it will subscribe to the Store to perform the updates when the store changes
   override public init(store: Store<V.VM.S>, connected: Bool = false) {
     super.init(store: store, connected: connected)
     // if the ViewControllerWithLocalState is not connected to the state when created, we still need to retrieve the local state
@@ -34,19 +147,21 @@ open class ViewControllerWithLocalState<V: ViewControllerModellableView & UIView
     }
   }
   
-  /// we are not using storyboards so trigger a fatalError
+  /// We are not using storyboards so trigger a fatalError
   public required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  /// we are about to unsubscribe from the global state, save it locally in the lastKnownState
-  /// while we are disconnected we will look at the lastKnownState
+  /// We are about to unsubscribe from the global state,
+  
+  // save it locally in the lastKnownState
+  // while we are disconnected we will look at the lastKnownState
   open override func willUnsubscribe() {
     self.lastKnownState = self.state
   }
   
-  /// executed during the viewWillAppear()
-  open override func warmUp() {
+  /// WarmUp phase, check if we should connect to the state
+  override func warmUp() {
     // we are using silent = true because we don't want to trigger two updates
     // one after the subscribing and one after the localStateDidChange()
     if self.shouldConnectWhenVisible {
@@ -56,21 +171,21 @@ open class ViewControllerWithLocalState<V: ViewControllerModellableView & UIView
     self.localStateDidChange()
   }
  
-  /// handle the state update, create a new updated viewModel and feed the view with that
+  /// Handle the state update, create a new updated viewModel and feed the view with that
   override func update(with state: V.VM.S) {
     // update the view model using the new state available
     // note that the updated method should take into account the local state that should remain untouched
      self.viewModel = V.VM(state: state, localState: self.localState)
  }
   
-  /// this method is called every time the local state changes
+  /// This method is called every time the local state changes
   private func localStateDidChange() {
     mainThread {
       self.updateLocalState(with: self.localState)
     }
   }
   
-  // handle the local state update
+  /// Handle the local state update
   private func updateLocalState(with localState: V.VM.LS) {
     let state = self.connected ? self.state : self.lastKnownState
     self.viewModel = V.VM(state: state, localState: localState)
