c4f0863debaa0a265edb8c77a6e132367e0d1f40
Andrea De Angelis <smaramba83@gmail.com>
2018-04-20 13:22:40 +0200

    Add example of a test with hooks

--- DemoTests/DemoTests.swift
+++ DemoTests/DemoTests.swift
@@ -11,15 +11,25 @@ import Tempura
 import Katana
 
 
-class UITests: XCTestCase {
+class ScreenTests: XCTestCase {
   
   func testAddItemScreen() {
 
+    /// simple test with one ViewModel
     test(AddItemView.self, with: AddItemViewModel(editingText: "this is a test"), container: .none, identifier: "addItem01")
     
+    /// multiple test with two ViewModel, this will produce two distinct screenshots
     test(AddItemView.self, with: ["addItem02": AddItemViewModel(editingText: "this is another test"),
                                   "addItem03": AddItemViewModel(editingText: "what about this?")],
          container: .none)
+    
+    test(AddItemView.self,
+         with: AddItemViewModel(editingText: "this is a test with hooks"),
+         container: .none,
+         identifier: "addItem04",
+         hooks: [UITests.Hook.viewDidLoad: { view in
+          view.viewController?.automaticallyAdjustsScrollViewInsets = true
+        }])
   }
   
 }
