194aad4b41a2e98ae7118ff7e9ccc3ef1c5d7648
Mauro Bolis <bolismauro@gmail.com>
2018-05-07 18:10:05 +0200

    Add documentation

--- Tempura/UITests/AsyncUITest.swift
+++ Tempura/UITests/AsyncUITest.swift
@@ -9,14 +9,39 @@ import Foundation
 import XCTest
 import Tempura
 
+/**
+ AsyncUITest is a more complex form of UITest that is used when the UI cannot be rendered immediately.
+ This happens for instance when things that are shown in the screen are taken from a remote server.
+ 
+ The idea is that the view is rendered but the system waits until `isViewReady` returns true to take the snapshot
+ and pass to the next test case. `isViewReady` is invoked various times with the view instance. The method should be implemented
+ so that it checks possible things that may not be ready yet and return true only when the view is ready to be snapshotted.
+ 
+ Note that this is a protocol as XCode fails to recognize subclasses of XCTestCase's subclasses that are written in Swift.
+*/
 public protocol AsyncUITest {
   associatedtype V: UIView & ViewControllerModellableView
   
+  /**
+   Add a new UI test to be performed
+   
+   - parameter model: the view model with which the view is created
+   - parameter identifier: a string identifier that is used to name the snapshot file
+   - parameter container: a parameter that specify in which container the view will be embedded
+   - parameter hooks: some hooks that can be added to customize the view after its creation
+   - parameter size: the size of the view
+  */
   func uiTest(model: V.VM, identifier: String, container: UITests.Container, hooks: [UITests.Hook: UITests.HookClosure<V>], size: CGSize)
+  
+  /**
+   Method used to check whether the view is ready to be snapshotted
+   - parameter view: the view that will be snapshotted
+  */
   func isViewReady(_ view: V) -> Bool
 }
 
 public extension AsyncUITest where Self: XCTestCase {
+  /// Default implementation for XCTestCase subclasses
   func uiTest(model: V.VM, identifier: String, container: UITests.Container, hooks: [UITests.Hook: UITests.HookClosure<V>], size: CGSize) {
     let snapshotConfiguration = UITests.ScreenSnapshot<V>(type: V.self, container: container, models: [identifier: model], hooks: hooks, size: size)
     let viewControllers = snapshotConfiguration.renderingViewControllers
@@ -45,6 +70,7 @@ public extension AsyncUITest where Self: XCTestCase {
 }
 
 public extension AsyncUITest {
+  /// The default implementation returns true
   func isViewReady(_ view: V) -> Bool {
     return true
   }
