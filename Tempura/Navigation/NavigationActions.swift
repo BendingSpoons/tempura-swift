15f9eb65da34fd70c1f11f7d71ff49f5cf3c4e25
Andrea De Angelis <smaramba83@gmail.com>
2018-10-26 15:00:01 +0200

    Update navigation side effects

--- Tempura/Navigation/NavigationActions.swift
+++ Tempura/Navigation/NavigationActions.swift
@@ -10,15 +10,10 @@ import Foundation
 import Katana
 
 /// Navigation action used to ask the `Navigator` to navigate to a specific `Route`.
-public struct Navigate: Action, ActionWithSideEffect {
+public struct Navigate: AnySideEffect {
   var route: Route
   var animated: Bool
   var context: Any?
-  
-  /// Returns the new state after the action is dispatched.
-  public func updatedState(currentState: State) -> State {
-    return currentState
-  }
   
   /// Initializes and return a Navigate action.
   public init(to route: Route, animated: Bool = false, context: Any? = nil) {
@@ -29,11 +24,10 @@ public struct Navigate: Action, ActionWithSideEffect {
   
   /// The side effect of the action, look into [Katana](https://github.com/BendingSpoons/katana-swift)
   /// to know what a `SideEffect` is.
-  public func sideEffect(currentState: State, previousState: State, dispatch: @escaping StoreDispatch, dependencies: SideEffectDependencyContainer) {
-    guard let dependencies = dependencies as? NavigationProvider else { fatalError("DependenciesContainer must conform to `NavigationProvider`") }
+  public func sideEffect(_ context: AnySideEffectContext) throws {
+    guard let dependencies = context.anyDependencies as? NavigationProvider else { fatalError("DependenciesContainer must conform to `NavigationProvider`") }
     dependencies.navigator.changeRoute(newRoute: self.route, animated: self.animated, context: self.context)
   }
-  
 }
 
 /// Navigation action used to ask the `Navigator` to show a specific screen
@@ -41,15 +35,10 @@ public struct Navigate: Action, ActionWithSideEffect {
 ///
 /// The `ViewController` that is managing that screen must implement `RoutableWithConfiguration`
 /// or `Routable` in order to be identified with a matching `Routable.routeIdentifier`.
-public struct Show: Action, ActionWithSideEffect {
+public struct Show: AnySideEffect {
   var identifiersToShow: [RouteElementIdentifier]
   var animated: Bool
   var context: Any?
-  
-  /// Returns the new state after the action is dispatched.
-  public func updatedState(currentState: State) -> State {
-    return currentState
-  }
   
   /// Initializes and return a Show action.
   public init(_ identifiersToShow: [RouteElementIdentifier], animated: Bool = false, context: Any? = nil) {
@@ -58,18 +47,18 @@ public struct Show: Action, ActionWithSideEffect {
     self.context = context
   }
   
-  /// Initializes and return a Show action.
+  /// Initializes and returns a Show action.
   public init(_ identifierToShow: RouteElementIdentifier, animated: Bool = false, context: Any? = nil) {
     self.init([identifierToShow], animated: animated, context: context)
   }
   
-  /// Initializes and return a Show action.
+  /// Initializes and returns a Show action.
   public init<K>(_ identifiersToShow: [K], animated: Bool = false, context: Any? = nil)
     where K: RawRepresentable, K.RawValue == RouteElementIdentifier {
       self.init(identifiersToShow.map { $0.rawValue }, animated: animated, context: context)
   }
   
-  /// Initializes and return a Show action.
+  /// Initializes and returns a Show action.
   public init<K>(_ identifierToShow: K, animated: Bool = false, context: Any? = nil)
     where K: RawRepresentable, K.RawValue == RouteElementIdentifier {
       self.init(identifierToShow.rawValue, animated: animated, context: context)
@@ -77,8 +66,8 @@ public struct Show: Action, ActionWithSideEffect {
   
   /// The side effect of the action, look into [Katana](https://github.com/BendingSpoons/katana-swift)
   /// to know what a `SideEffect` is.
-  public func sideEffect(currentState: State, previousState: State, dispatch: @escaping StoreDispatch, dependencies: SideEffectDependencyContainer) {
-    guard let dependencies = dependencies as? NavigationProvider else { fatalError("DependenciesContainer must conform to `NavigationProvider`") }
+  public func sideEffect(_ context: AnySideEffectContext) throws {
+    guard let dependencies = context.anyDependencies as? NavigationProvider else { fatalError("DependenciesContainer must conform to `NavigationProvider`") }
     dependencies.navigator.show(self.identifiersToShow, animated: self.animated, context: self.context)
   }
   
@@ -89,16 +78,11 @@ public struct Show: Action, ActionWithSideEffect {
 ///
 /// The `ViewController` that is managing that screen must implement `RoutableWithConfiguration`
 /// or `Routable` in order to be identified with a matching `Routable.routeIdentifier`.
-public struct Hide: Action, ActionWithSideEffect {
+public struct Hide: AnySideEffect {
   var identifierToHide: RouteElementIdentifier
   var animated: Bool
   var context: Any?
   var atomic: Bool
-  
-  /// Returns the new state after the action is dispatched.
-  public func updatedState(currentState: State) -> State {
-    return currentState
-  }
   
   /// Initializes and return a Hide action.
   public init(_ identifierToHide: RouteElementIdentifier, animated: Bool = false, context: Any? = nil, atomic: Bool = false) {
@@ -123,8 +107,8 @@ public struct Hide: Action, ActionWithSideEffect {
   
   /// The side effect of the action, look into [Katana](https://github.com/BendingSpoons/katana-swift)
   /// to know what a `SideEffect` is.
-  public func sideEffect(currentState: State, previousState: State, dispatch: @escaping StoreDispatch, dependencies: SideEffectDependencyContainer) {
-    guard let dependencies = dependencies as? NavigationProvider else { fatalError("DependenciesContainer must conform to `NavigationProvider`") }
+  public func sideEffect(_ context: AnySideEffectContext) throws {
+    guard let dependencies = context.anyDependencies as? NavigationProvider else { fatalError("DependenciesContainer must conform to `NavigationProvider`") }
     dependencies.navigator.hide(self.identifierToHide, animated: self.animated, context: self.context, atomic: self.atomic)
   }
   
