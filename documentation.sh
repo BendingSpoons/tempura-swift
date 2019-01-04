c4a0863ed0e252b4ff7df75400f4f85b8fdfd0ff
Andrea De Angelis <smaramba83@gmail.com>
2019-01-04 10:54:00 +0100

    Add documentation

---
+++ documentation.sh
@@ -0,0 +1,6 @@
+jazzy \
+  --author "Bending Spoons Team" \
+  --author_url http://bendingspoons.com \
+  --github_url https://github.com/BendingSpoons/tempura-swift \
+  --github-file-prefix https://github.com/BendingSpoons/tempura-swift/blob/master/ \
+  --module Tempura
