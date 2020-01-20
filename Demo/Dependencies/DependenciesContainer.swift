0d8fbe930f5685179677fb54998ad3bb3c1d6c3f
albertogiunta <alb.giunta@gmail.com>
2020-01-20 16:42:38 +0100

    fix: dependencies container

--- Demo/Dependencies/DependenciesContainer.swift
+++ Demo/Dependencies/DependenciesContainer.swift
@@ -10,31 +10,15 @@ import Katana
 import Tempura
 
 final class DependenciesContainer: NavigationProvider {
-  let promisableDispatch: PromisableStoreDispatch
-  
-  var getAppState: () -> AppState
-    
+  var dispatch: Dispatch
+  var getState: GetState
   var navigator: Navigator = Navigator()
   
-  var getState: () -> State {
-    return self.getAppState
-  }
-  
-  init(dispatch: @escaping PromisableStoreDispatch, getAppState: @escaping () -> AppState) {
-    self.promisableDispatch = dispatch
-    self.getAppState = getAppState
-  }
-}
-
-extension DependenciesContainer {
-  convenience init(dispatch: @escaping PromisableStoreDispatch, getState: @escaping GetState) {
-    let getAppState: () -> AppState = {
-      guard let state = getState() as? AppState else {
-        fatalError("Wrong State Type")
-      }
-      return state
-    }
-    
-    self.init(dispatch: dispatch, getAppState: getAppState)
+  required init(
+    dispatch: @escaping SideEffectDependencyContainer.Dispatch,
+    getState: @escaping GetState
+  ) {
+    self.dispatch = { dispatchable in dispatch(dispatchable).void }
+    self.getState = getState
   }
 }
